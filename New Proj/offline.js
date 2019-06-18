184522f174fa89ba2c221f713fa188f5a2614bd4
Erednasyl <38250121+Erednasyl@users.noreply.github.com>
2019-06-17 21:42:39 -0300

    Add files via upload

---
+++ New Proj/offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1560818483,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/char-sheet0.png",
+		"images/shootbutton-sheet0.png",
+		"images/shootbutton-sheet1.png",
+		"images/bulletcontroller-sheet0.png",
+		"images/setacima-sheet0.png",
+		"images/setacima-sheet1.png",
+		"images/sprite-sheet0.png",
+		"images/dbspqcw100388da5aac45ec87988e1bdfacdb6c-sheet0.png",
+		"images/spritefont.png",
+		"images/powerupcontroller-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
