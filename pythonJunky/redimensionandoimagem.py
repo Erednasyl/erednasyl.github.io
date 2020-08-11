8342b847cef3b45bb8b1f56df3dd670d284a5c92
Erednasyl <38250121+Erednasyl@users.noreply.github.com>
2020-08-10 21:17:31 -0300

    Add files via upload

--- pythonJunky/redimensionandoimagem.py
+++ pythonJunky/redimensionandoimagem.py
@@ -1,7 +1,10 @@
 from PIL import Image
 from resizeimage import resizeimage
-
-with open('test-image.png', 'r+b') as f:
-    with Image.open(f) as image:
-        cover = resizeimage.resize_width(image, 512)
-        image.save('pugizinhopronto.png', image.format)
+for index in range (0,39):
+    img = '/workspace/erednasyl.github.io/pythonJunky/{}.png'
+    with open(img.format(209523126+index), 'r+b') as f:
+        with Image.open(f) as image:
+            #cover = resizeimage.resize_width(image, 50)
+            cover = resizeimage.resize_contain(image, [512, 320])
+            arquivopronto ='/workspace/erednasyl.github.io/pythonJunky/arquivosprontos/pugizinhopronto{}.png'
+            cover.save(arquivopronto.format(index), image.format)
